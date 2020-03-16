f880e504d2db7c06b2d9d4c016f18a397fb90770
Chemi-F <ryo.u.z.0429@gmail.com>
2020-03-16 17:04:43 +0900

    update

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 #for vim
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
 
 #for neovim
-#curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-#mkdir -p ~/.config/nvim
-#ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+mkdir -p ~/.config/nvim
+ln -sf ~/dotfiles/.vimrc ~/.config/nvim/init.vim
+
+
+#ln -sf ~/dotfiles/.latexmkrc ~/.latexmkrc
