ed803297f1a023e95e2026cd4f908abae5ff9345
Chemi-F <ryo.u.z.0429@gmail.com>
2020-09-24 15:12:42 +0900

    Add memo

--- install.sh
+++ install.sh
@@ -3,13 +3,13 @@
 #for vim
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-ln -sf ~/dotfiles/vimrc ~/.vimrc
+ln -sf ./.vimrc ~/.vimrc
 
 #for neovim
 #curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
-        #https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 #mkdir -p ~/.config/nvim
-#ln -sf ~/dotfiles/vimrc ~/.config/nvim/init.vim
+#ln -sf ./.vimrc ~/.config/nvim/init.vim
 
 #for latexmk
-#ln -sf ~/dotfiles/latexmkrc ~/.latexmkrc
+#ln -sf ./.latexmkrc ~/.latexmkrc
